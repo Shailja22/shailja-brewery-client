cb66a59577dc347563ada38cf7c9e64aa2df025c
Raushan kumar <raushangnit@gmail.com>
2023-10-04 23:01:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/shailjabreweryclient/ShailjaBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.shailjabreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShailjaBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShailjaBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shailjabreweryclient/ShailjaBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shailjabreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShailjaBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
